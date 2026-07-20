7a45f1568f16d8a6a77412bd2af745d1a1aac778
Gururaj760339 <www.gururaj555@gmail.com>
2026-07-20 14:48:53 +0600

    test car rental review section

--- resources/views/car_rental.blade.php
+++ resources/views/car_rental.blade.php
@@ -302,7 +302,7 @@
 
     </section>
 
-    @if ($rental_booking && $rental_booking->status == 'Completed')
+    @if (Auth::check() && $rental_booking && $rental_booking->status == 'Completed')
         <div class="max-w-2xl mx-auto mt-10">
             <div class="bg-white rounded-2xl shadow-xl border border-gray-200 p-8">
 
